abba13fb3dd03041a275bf7aa927a7ef5ab1d94e
zhangwentao <zhangwentao@qtone.cn>
2017-02-28 11:41:22 +0800

    上传项目

---
+++ dist/define/qkydata.js
@@ -0,0 +1,24 @@
+
+/****
+默认数据存放
+****/ 
+
+define({
+	navdata:{/*导航数据*/
+		"logosrc":"images/nav_logo/logo.png",
+		"pjname":"初始页面",
+		"navli":[["导航一","href='#'"],["导航二","href='#'"],["导航三","href='#'"],["导航四","href='#'"]],
+		"navli_active":0,
+		"moreli":{
+			"校园办公":["xiaoyuanbangong",[["个人办公","href='#'"],["行政办公","href='#'"],["流程审批","href='#'"]]],
+			"校园支付":["xiaoyuanzhifu",[["个人办公","href='#'"],["行政办公","href='#'"],["流程审批","href='#'"]]],
+			"智慧教学":["zhihuijiaoxue",[["个人办公","href='#'"],["行政办公","href='#'"],["流程审批","href='#'"]]]
+		},
+		"morebtn":true,
+		"common":[["个人办公","href='#'"],["行政办公","href='#'"],["流程审批","href='#'"]],
+		"lately":[["个人办公","href='#'"],["行政办公","href='#'"],["流程审批","href='#'"]],
+		"isinfo":true,//是否支持登录显示个人信息
+		"tea_info":{"name":"张晓明","isphoto":true,"photo":"images/tx01.png"},
+		"otherli":[["个人空间","href='#'"],["退出","id='toOut'"]]
+	}
+});
